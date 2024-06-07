0898280f988eaec7d9445662326a3d6eafd2988e
AhsanHayat7 <ahsanhayat9071@gmail.com>
2024-06-07 06:04:04 +0500

    learning => PHP=>arrays,assignment_operators,associative_array,comparison_operators,Do_while_loop,for_loops,foreach_loop,functions_arguments,function_parameters,functions_return,functions,Global_local,Goto_statement,if_else,_elseif,if_statement_ifelse,logical_operators,multidimensional_arrays,nested_loops,Recursive_function,string_operator,switch_statement,Tenary_Operator,variable_function,while_loop.

---
+++ functions_arguments.php
@@ -0,0 +1,46 @@
+<?php
+
+
+// fucntion by reference vlaue
+function wow($name){
+    $name = "Laravel";
+
+}
+
+$language = "PHP";
+
+wow($language);
+echo $language;
+
+
+// function reference by agrument
+function cricket(&$batsman){
+
+    $batsman = "LAla"; 
+        
+}
+echo "<br>";
+$bowler = "amir";
+cricket($bowler);
+echo $bowler;
+
+echo "<br>";
+function first($number){  //function  by value
+    $number +=10; 
+}
+
+
+function second(&$number){ // function by reference 
+    $number +=6;
+}
+
+
+$num = 8;
+first($num);
+echo "The original value is : " .$num;
+echo "<br>";
+second($num);
+echo "The original value is : " .$num;
+
+
+?>
---
+++ multidimensional_array.php
@@ -0,0 +1,48 @@
+<?php
+
+
+$data= [
+
+//   0     1          2       3
+    [1,"Mike tyson","Boxer",25000000], // 0
+    [2,"Muhammad Ali","Goat",100000000], // 1
+    [3,"khabib","Fighter",3500900],    //  2
+    [4,"Islam","Champion",50000000]  //  3
+];
+
+echo "<pre>";
+print_r($data);
+echo "</pre>";
+
+
+
+echo $data[1][1];
+
+echo "<br>";
+
+for ($row= 0; $row< 4; $row++){
+    for( $col= 0; $col< 4; $col++){
+        echo $data[$row][$col] . " ";
+}
+    echo "<br>";
+}
+
+
+echo "<Table border='2px' cellpadding='5px' cellspacing='0' >";
+echo "<tr>
+        <th>User Id.</th>
+        <th>Name.</th>
+        <th>Designation.</th>
+        <th>Salary.</th>
+      </tr>";    
+
+foreach($data as $d1){
+    echo "<tr>";
+    foreach($d1 as $d2){
+        echo "<td> $d2 </td>";
+    }
+
+    echo "</tr>";
+}
+echo "</table>";
+?>
